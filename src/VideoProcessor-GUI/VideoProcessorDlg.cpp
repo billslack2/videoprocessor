3821f5e41f7627dfd5293c7353d7ccb0b4995c95
Enchy <113152886+enchywastaken@users.noreply.github.com>
2023-01-19 16:56:38 -0800

    Added keyboard shortcuts to set transfer function to PQ (ctrl_shift+p) or Auto (ctrl+shift+a)

--- src/VideoProcessor-GUI/VideoProcessorDlg.cpp
+++ src/VideoProcessor-GUI/VideoProcessorDlg.cpp
@@ -781,20 +781,14 @@ void CVideoProcessorDlg::OnCommandRendererReset()
 void CVideoProcessorDlg::OnCommandPQSet()
 {
 	m_rendererTransferFunctionCombo.SetCurSel(1);
-	if (m_videoRenderer)
-	{
-		m_videoRenderer->Reset();
-	}
+	OnBnClickedRendererRestart();
 }
 
 
 void CVideoProcessorDlg::OnCommandAutoSet()
 {
 	m_rendererTransferFunctionCombo.SetCurSel(0);
-	if (m_videoRenderer)
-	{
-		m_videoRenderer->Reset();
-	}
+	OnBnClickedRendererRestart();
 }
 
 //
