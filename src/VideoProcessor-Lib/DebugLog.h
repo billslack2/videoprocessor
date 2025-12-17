ddfdb1a471df301970e82675bae9e981d1d70f1a
billslack2 <bslack@gmail.com>
2025-12-17 08:18:47 -0500

    pure rational timeline

--- src/VideoProcessor-Lib/DebugLog.h
+++ src/VideoProcessor-Lib/DebugLog.h
@@ -1,5 +1,5 @@
 /*
- * Copyright(C) 2021 Dennis Fleurbaaij <mail@dennisfleurbaaij.com>
+ * Copyright(C) 2025 Bill Slack <bslack@gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 3.
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
